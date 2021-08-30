9149d8a58b311350a59c1702077213e85d1b2cb2
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-08-29 23:44:22 -0300

    Project Creation

---
+++ src/test/kotlin/com/felipe/booking/alten/AltenApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.felipe.booking.alten
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AltenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
