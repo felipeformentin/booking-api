a60e9a73f79c9987e838d367a9f988de787ffae8
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-08-30 17:54:13 -0300

    Adding message for optmistic lock

--- src/main/java/com/felipe/booking/app/config/routing/ServerRouting.java
+++ src/main/java/com/felipe/booking/app/config/routing/ServerRouting.java
@@ -5,6 +5,7 @@ import com.felipe.booking.app.handler.BookingHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.web.reactive.function.server.HandlerFilterFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -30,6 +31,8 @@ public class ServerRouting {
 
     private HandlerFilterFunction<ServerResponse, ServerResponse> badRequestHandler() {
         return (request, next) -> next.handle(request)
+                .onErrorResume(OptimisticLockingFailureException.class,
+                        e -> ServerResponse.badRequest().bodyValue(new ValidationErrorDTO("Someone was booking the room at the same time. Please try again later.")))
                 .onErrorResume(ResponseStatusException.class,
                         e -> ServerResponse.badRequest().bodyValue(new ValidationErrorDTO(e.getReason())));
     }
