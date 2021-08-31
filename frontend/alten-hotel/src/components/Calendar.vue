a3e7bafe2674ef1a1d2e1746d1ca30e063d7b9d8
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-08-31 19:25:31 -0300

    Adding disabled days

--- frontend/alten-hotel/src/components/Calendar.vue
+++ frontend/alten-hotel/src/components/Calendar.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="text-center section">
-
-    <v-calendar
-    :min-date="minimumDate()"
-    :max-date="maximumDate()"
+    <v-date-picker
+      v-model="user"
+      is-range
+      :min-date="minimumDate()"
+      :max-date="maximumDate()"
+      :disabled-dates="disabledDays"
     />
-     
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  name: "Calendar",
+  data() {
+    return {
+      disabledDays: [],
+      user: {},
+    };
+  },
+  created() {
+    this.getDisabledDays();
+  },
   methods: {
     minimumDate() {
       var today = new Date();
@@ -19,8 +32,19 @@ export default {
     maximumDate() {
       var today = new Date();
       return today.setDate(today.getDate() + 30);
-    }
-  }
+    },
+    getDisabledDays() {
+      axios
+        .get("http://localhost:8080/availability")
+        .then((res) => {
+          res.data.daysBooked.forEach(day => {
+            this.disabledDays.push(new Date(day[0], day[1] - 1, day[2]));
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
-
 </script>
